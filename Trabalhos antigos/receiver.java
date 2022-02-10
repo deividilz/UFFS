00980938c857990cc5fe2778a3bc4d3b7de627cb
Deividi <deividi96_luiz@hotmail.com>
2022-02-09 21:38:26 -0300

    Att

--- Trabalhos antigos/receiver.java
+++ Trabalhos antigos/receiver.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 
@@ -14,7 +15,21 @@ public class receiver {
             byte[] dados = new byte[100];
             DatagramPacket pacote = new DatagramPacket(dados, dados.length);
             msocket.receive(pacote);
-            System.out.println("Mensagem: " + new String(pacote.getData(), 0, pacote.getLength()));
+            System.out.println("Mensagem: " + new String(pacote.getData(), 0, pacote.getLength())); 
+
+            DatagramPacket pacote2 = new DatagramPacket(dados, dados.length, grupo, porta);
+            System.out.println("Porta: " + dados + "Int: " + porta );
+            msocket.send(pacote2);
+            System.out.println("cacetinho");
         }
+
+        /*try(DatagramSocket socket = new DatagramSocket()){
+            String mensagem = Integer.toString(porta);
+            byte[] dados = mensagem.getBytes();
+            InetAddress grupo = InetAddress.getByName("230.1.2.3");
+            DatagramPacket pacote = new DatagramPacket(dados, dados.length, grupo, porta);
+            System.out.println("Porta: " + dados + "Int: " + porta );
+            socket.send(pacote);
+        }*/
     }
 }
--- Trabalhos antigos/sender.java
+++ Trabalhos antigos/sender.java
@@ -2,13 +2,15 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.net.SocketAddress;
 import java.net.SocketException;
 
 public class sender {
-    public static void main(String[] args) throws SocketException{
+    public static void main(String[] args) throws IOException{
         try (DatagramSocket socket = new DatagramSocket()) {
             InetAddress grupo = InetAddress.getByName("230.1.2.3");
-            String mensagem = "Hello";
+            String mensagem = "Qual é o seu endereço de IP?";
             byte[] dados = mensagem.getBytes();
             int porta = 5000;
             DatagramPacket pacote = new DatagramPacket(dados, dados.length, grupo, porta);
@@ -17,6 +19,25 @@ public class sender {
             throw e;
         } catch (IOException e) {
             e.printStackTrace();
+        }
+
+        int porta = 5000;
+        try (MulticastSocket msocket = new MulticastSocket(porta)) {
+            System.out.printf("Teste\n");
+            InetAddress grupo = InetAddress.getByName("230.1.2.3");
+            msocket.joinGroup(grupo);
+            System.out.printf("Teste2\n");
+            byte[] dados = new byte[100];
+            DatagramPacket pacote = new DatagramPacket(dados, dados.length);
+            System.out.printf("teste3");
+            msocket.receive(pacote);
+            try{
+                System.out.println("Mensagem retornada: " + new String(pacote.getData(), 0, pacote.getLength())); 
+            }
+            catch (Exception e){
+                System.out.println("Mensagem retornada");
+            }
+            
         }
     }
 }
